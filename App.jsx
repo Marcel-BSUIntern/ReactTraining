0291bf5e397f588c5050ed78eaf2af65c4c1c445
Marcel-BSUIntern <s.marcelbautista@gmail.com>
2025-02-19 13:06:12 +0800

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import UserGreeting from "./UserGreeting.jsx";
+function App() {
+  return(
+    <>
+        <UserGreeting isLoggedIn={true} username="Marcel"/>
+    </>
+  );
+}
+
+export default App
